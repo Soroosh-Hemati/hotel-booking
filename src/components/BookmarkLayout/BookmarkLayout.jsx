a40bbee2589f4c18265be69ca6fed185404009c5
Soroosh-Hemati <sorooshhemati4@gmail.com>
2024-09-24 11:28:59 +0330

    bookmark context added

--- src/components/BookmarkLayout/BookmarkLayout.jsx
+++ src/components/BookmarkLayout/BookmarkLayout.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import Map from '../Map/Map'
+import { Outlet } from 'react-router-dom'
 
-function Bookmark() {
+function BookmarkLayout() {
     return (
         <div className='appLayout'>
             <div className="sidebar">
-                {/* <Outlet/> */}
-                <div>bookmarkList</div>
+                <Outlet />
             </div>
-            <Map markerLocations={[]}/>
+            <Map markerLocations={[]} />
         </div>
     )
 }
 
-export default Bookmark
+export default BookmarkLayout
---
+++ src/components/context/BookmarkListContext.jsx
@@ -0,0 +1,42 @@
+import React, { useContext, useState } from 'react'
+import { createContext } from 'react'
+import useFetch from '../../hooks/useFetch'
+import toast from 'react-hot-toast';
+import axios from 'axios';
+
+const HotelContext = createContext();
+const BASE_URL = 'http://localhost:5000';
+
+function BookmarkListProvider({ children }) {
+    const [currentBookmark, setCurrentBookmark] = useState(null);
+    const [isLoadingCurrBookmark, setIsLoadingCurrBookmark] = useState(false);
+
+
+
+    const { isLoading, data: bookmarks } = useFetch(`${BASE_URL}/bookmark`);
+
+    async function getBookmark(id) {
+        setIsLoadingCurrBookmark(true)
+        try {
+            const { data } = await axios.get(`${BASE_URL}/bookmark/${id}`)
+            setCurrentBookmark(data)
+            setIsLoadingCurrBookmark(false)
+        } catch (error) {
+            toast.error(error.message)
+            setIsLoadingCurrBookmark(false);
+        }
+    }
+
+
+    return (
+        <BookmarkContext.Provider value={{ isLoading, bookmarks, currentBookmark, getBookmark, isLoadingCurrBookmark }}>
+            {children}
+        </BookmarkContext.Provider>
+    )
+}
+
+export default BookmarkListProvider
+
+export function useBookmark() {
+    return useContext(HotelContext)
+}
